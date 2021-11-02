fd4696aee17f2f5f31998a3e3fd4a069ff8baff9
Mitch Lillie <mitch.lillie@moogsoft.com>
2021-11-02 16:33:51 -0700

    Vercel fixes

--- src/movie-service.js
+++ src/movie-service.js
@@ -1,5 +1,5 @@
 export const getMovies = function (query) {
-    const url = `http://www.omdbapi.com/?apikey=e7b6c314&type=movie&s=${query}`;
+    const url = `https://www.omdbapi.com/?apikey=e7b6c314&type=movie&s=${query}`;
 
     return fetch(url, {
         method: 'GET',
